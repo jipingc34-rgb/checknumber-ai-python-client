4b778985d76faef1d1d56720a5901670f7d701a1
checknumber <jipingc34@gmail.com>
2026-01-19 17:22:20 +0800

    Update run_check.bat
    Troubleshooting: Dependencies Installation & Chinese Text Recognition/Encoding Issues

--- run_check.bat
+++ run_check.bat
@@ -1,5 +1,15 @@
 @echo off
-echo 正在启动 WhatsApp 号码检测工具...
+chcp 65001
+echo ==============================================
+echo 正在自动安装脚本所需依赖包...
+echo ==============================================
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests --upgrade
+echo.
+echo ==============================================
+echo 依赖安装完成，正在运行号码检测脚本...
+echo ==============================================
 python whatsapp_check.py
-echo 按任意键退出...
-pause >nul
+echo.
+echo ==============================================
+echo 脚本运行结束，按任意键退出...
+pause > nul
